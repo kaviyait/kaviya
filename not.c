ff5f14f148e713ff9532c79f4a810fcfab4776a8
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-21 18:32:14 +0530

    Create not.c

---
+++ not.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main();
+{
+int n;
+printf("enter the year");
+if(n%4==0)
+{
+if(n%100==0)
+{
+if(n%400==0)
+{
+printf("the year is leap year");
+}
+else
+{
+printf("the year is not leap year");
+}
+printf("the year is leap year");
+}
+else
+{printf("the year is not leap year");
+}
+
+return 0;
+}}
+}
+}
