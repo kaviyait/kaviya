eebe3fef76d278ae87704516085e23227aaf8e42
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-21 17:09:23 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number");
+if(num<0)
+{
+printf("the number is positive");
+}
+else
+{
+printf("the number is negative");
+}
+return 0;
+}
