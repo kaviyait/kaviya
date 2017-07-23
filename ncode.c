0dc1cca1f2d14deb33b3ec0fa0fd9954c94278fd
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-23 13:49:06 +0530

    ncode.c

---
+++ ncode.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+	 
+	char* encode(char* str2) {
+	   int i = 0;
+	 
+	   while (str2[i] != '\0') {
+	      str1[i] = str2[i] - 30;  // Subtract 30 From Charcter
+	      i++;
+	   }
+	   return (str2);
+	}
+	 
+	int main() {
+	 
+	   char *str2;
+	 
+	   printf("\nEnter the String to be Encode : ");
+	   scanf("%d",&str2);
+	 
+	   str1 = encode(str2);
+	   printf("\nEncoded String : %s", str2);
+	 
+	   return 0;
+	}
