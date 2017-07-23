0b6f72e7ce9906d7dc26eaff8e3240fd604527fb
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-23 16:39:22 +0530

    Create pal.c

---
+++ pal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n, revInt = 0, rem, origInt;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    origInt = n;
+    while( n!=0 )
+    {
+        rem = n%10;
+        revInt = revInt*10 + rem;
+        n /= 10;
+    }
+    if (origInt == revInt)
+        printf("%d is a palindrome.", origInt);
+    else
+        printf("%d is not a palindrome.", origInt);
+    
+    return 0;
+}
