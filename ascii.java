ed045bf08233df23624b6e4a0c51662deae68e8e
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-25 13:28:19 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,18 @@
+import.java.io.*;
+import.java.util.*;
+public class Ascii {
+
+  public static void main (String[]args) {
+
+   for (int c=32; c<123; c++) {
+
+    System.out.print((char)c);
+
+ 
+
+ 
+
+   }
+ }
+
+}
