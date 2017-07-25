7716c5d615752c300b012cc073a574733748f653
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-25 11:02:48 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,12 @@
+    import.java.io.*;
+    import.java.util.*;
+    class FactorialExample{  
+     public static void main(String args[]){  
+      int i,f=1;  
+      int number=5;
+      for(i=1;i<=number;i++){    
+          f=f*i;    
+      }    
+      System.out.println("Factorial of "+number+" is: "+f);    
+     }  
+    }  
