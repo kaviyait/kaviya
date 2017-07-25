427d23f31372ecc284273aa1e446814aec0803c3
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-25 09:38:19 +0530

    Create n.java

---
+++ n.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class SumOfNumbers
+{
+    public static void main(String arg[])
+    {
+        int input = 6;
+        int sum = 0;
+        
+        for(int i = 1; i <= input; i++)
+        {
+            sum = sum + i;   
+            System.out.println("Sum after adding " + i + " is : " + sum); 
+        }
+        
+        System.out.println();
+        System.out.println("Sum of numbers till " + input + " is " + sum); 
+    
+    }
+}
