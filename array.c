da5e4737add8eff8cd21e60afd089d18be25d27b
kaviyait <30313655+kaviyait@users.noreply.github.com>
2017-07-27 15:08:12 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void findSubarrays(int a[], int n)
+{
+    bool found = false;
+    int lsum = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        lsum += a[i];
+        int rsum = 0;
+        for (int j = i + 1; j < n; j++)
+            rsum += arr[j];
+ 
+        if (lsum*(n-i-1) == rsum*(i+1))
+        {
+            printf("From (%d  %d) to (%d  %d)\n", 0, i,
+                                            i+1, n-1);
+            found = true;
+        }
+    }
+ 
+    if (found == false)
+        cout << "Subarrays not found" << endl;
+}
+ 
+int main()
+{
+    int a[] = {1, 5, 7, 2, 0};
+    int n = sizeof(arr)/sizeof(a[0]);
+    findSubarrays(arr, n);
+    return 0;
+}
